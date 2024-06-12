4d198e415693972abdf20debe95fb3ccaa9cdf81
Mohammad-Rishad-p <r25richu@gmail.com>
2024-06-12 15:07:17 +0530

    updated basic widget

--- lib/basic_widgets/heading_text_widget.dart
+++ lib/basic_widgets/heading_text_widget.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:info_cab_u/constant.dart';
 
 class HText extends StatelessWidget {
-  const HText({super.key, required this.content});
+  const HText({super.key, required this.content, required this.textColor});
 
   final String content;
+  final Color textColor;
 
 
   @override
   Widget build(BuildContext context) {
-    return Text(content, style: TextStyle(color: buttonColor, fontSize: 30, fontWeight: FontWeight.w800),);
+    return Text(content, style: TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.w800),);
   }
 }
--- lib/basic_widgets/normal_text_widget.dart
+++ lib/basic_widgets/normal_text_widget.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class NText extends StatelessWidget {
-  const NText({super.key, required this.content});
+  const NText({super.key, required this.content,required this.textColor});
 
   final String content;
+  final Color textColor;
 
   @override
   Widget build(BuildContext context) {
-    return Text(content, style: TextStyle(color: Colors.black, fontSize: 20,),);
+    return Text(content, style: TextStyle(color: textColor, fontSize: 20,),);
   }
 }
