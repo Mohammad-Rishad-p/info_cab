048ce8644d45fd63d1c6d712557f765519f3de28
dilshankunnummal <dilshankunnummal@gmail.com>
2024-06-13 10:27:29 +0530

    otp 1st

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 const Color primaryColor = Colors.white;
 const Color SecondaryColor = Color(0xf2F3C7E);
-const Color buttonColor = Color(0xf787171);
+//const Color buttonColor = Color(0xf787171);
+const Color black = Colors.black;
+const Color textPrimColor = Color(0xFF2F3C7E);
+const Color textSecColor = Color(0xFF787171);
+const Color buttonColor = Color(0xFF2F3C7E);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:info_cab_u/pages/home_page.dart';
+import 'package:info_cab_u/pages/otp_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => HomePage(),
+        '/': (context) => OtpPage(),
       },
     );
   }
