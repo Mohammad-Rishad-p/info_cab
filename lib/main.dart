c936bb882f46e72da69ca06d8d2a668e92370c97
Mohammad-Rishad-p <r25richu@gmail.com>
2024-06-13 12:02:30 +0530

    added comments for otpPage

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/otppage',
       routes: {
         '/': (context) => HomePage(),
         '/register': (context) => UserRegisterPage(),
