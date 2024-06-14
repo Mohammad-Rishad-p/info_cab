64ee351baa8c8387c6ba530ee36622fb24a757f1
dilshankunnummal <dilshankunnummal@gmail.com>
2024-06-14 10:45:35 +0530

    editted textfield in home, register,

--- lib/pages/user/user_register_page.dart
+++ lib/pages/user/user_register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:info_cab_u/basic_widgets/button_widget.dart';
 import 'package:info_cab_u/basic_widgets/heading_text_widget.dart';
 import 'package:info_cab_u/constant.dart';
 
@@ -32,91 +33,138 @@ class _UserRegisterPageState extends State<UserRegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: primaryColor,
-      body:
-      Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-
-              // Heading Text
-              const HText(content: 'Provide', textColor:blackText),
-              const HText(content: 'Your Information', textColor: blueText),
-                const SizedBox(height: 45,),
-
-              //Text field for Phone Number
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: primaryColor,
+        body:
+        Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+      
+                // Heading Text
+                const HText(content: 'Provide', textColor:blackText),
+                const HText(content: 'Your Information', textColor: textPrimColor),
+                  const SizedBox(height: 45,),
+      
+                //Text field for Phone Number
+                  TextFormField(
+                    maxLength: 10,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                    labelText: 'Enter Your Mobile Number',
+                    prefixText: '+91 ',
+                    border: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: textSecColor, width: 2.0),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: textSecColor, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.redAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.redAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your mobile number';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 3.0),
+      
+                //text field for Name
                 TextFormField(
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                  labelText: 'Enter Your Mobile Number',
-                  prefixText: '+91 ',
-                  border: OutlineInputBorder(),
+                  decoration: const InputDecoration(
+                    labelText: 'Enter Your Name',
+                    border: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: textSecColor, width: 2.0),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: textSecColor, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.redAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.redAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your mobile number';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 25.0),
-
-              //text field for Name
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Enter Your Name',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 25.0),
+      
+                //Drop down box for company names
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(
+                    labelText: 'Enter Your Company Name',
+                    border: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: textSecColor, width: 2.0),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: textSecColor, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.redAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.redAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedCompany = newValue!;
+                    });
+                  },
+                  items: _companies.map((company) {
+                    return DropdownMenuItem(
+                      value: company,
+                      child: Text(company),
+                    );
+                  }).toList(),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your name';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 25.0),
-
-              //Drop down box for company names
-              DropdownButtonFormField<String>(
-                decoration: const InputDecoration(
-                  labelText: 'Enter Your Company Name',
-                  border: OutlineInputBorder(),
-                ),
-                onChanged: (newValue) {
-                  setState(() {
-                    _selectedCompany = newValue!;
-                  });
-                },
-                items: _companies.map((company) {
-                  return DropdownMenuItem(
-                    value: company,
-                    child: Text(company),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 40.0),
-
-              //Submit Button
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
+                const SizedBox(height: 25.0),
+      
+                //Submit Button
+                SizedBox(
+                  width: double.infinity,
+                  child: Button(onPressed: (){
                     if (_formKey.currentState!.validate()) {
                       // Process data
                     }
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(buttonColor),
-                    foregroundColor: MaterialStateProperty.all(primaryColor)
-                  ),
-                  child: const Text('Submit'),
+                  }, text: 'Submit')
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
