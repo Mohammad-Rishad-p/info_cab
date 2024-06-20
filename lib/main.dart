3b69bb0b7b7bfeb1e4134f8f8c6ad280e679500d
Mohammad-Rishad-p <r25richu@gmail.com>
2024-06-20 10:41:32 +0530

    m

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
+      initialRoute: '/userslist',
 
       routes: {
         '/': (context) => HomePage(),
